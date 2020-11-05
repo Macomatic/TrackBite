db7b79a77010a8c9cb70749c52c5c250a0b10eaa
Marco Vethanayagam <29697062+Macomatic@users.noreply.github.com>
2020-11-04 20:59:58 -0500

    Added pages

---
+++ app/src/main/java/com/example/diabeticmealtracker/ExerciseAnalysis_Page.java
@@ -0,0 +1,14 @@
+package com.example.diabeticmealtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExerciseAnalysis_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_analysis__page);
+    }
+}
---
+++ app/src/main/java/com/example/diabeticmealtracker/FoodGraph_Page.java
@@ -0,0 +1,14 @@
+package com.example.diabeticmealtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodGraph_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_graph__page);
+    }
+}
