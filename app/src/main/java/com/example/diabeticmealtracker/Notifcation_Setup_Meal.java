8a3590896e5d8ff88a97b24dde93a9cab4db7cd0
Andy <43015964+andyung17@users.noreply.github.com>
2020-11-04 20:58:42 -0500

    Added several pages that were relating to notifcation function

---
+++ app/src/main/java/com/example/diabeticmealtracker/Notifcation_Setup_Meal.java
@@ -0,0 +1,21 @@
+package com.example.diabeticmealtracker;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class Notifcation_Setup_Meal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifcation__setup__meal);
+
+    }
+}
---
+++ app/src/main/java/com/example/diabeticmealtracker/Notifcation_setup_exercise.java
@@ -0,0 +1,14 @@
+package com.example.diabeticmealtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Notifcation_setup_exercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifcation_setup_exercise);
+    }
+}
