c6144d8ed297bfc847a4bfe0bb98623aa3640438
FredWei-Hub <fredwei2001@gmail.com>
2020-11-14 06:23:28 -0500

    create field variables for basic input

--- app/src/main/java/com/example/diabeticmealtracker/Basic_Input.java
+++ app/src/main/java/com/example/diabeticmealtracker/Basic_Input.java
@@ -3,12 +3,37 @@ package com.example.diabeticmealtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Basic_Input extends AppCompatActivity {
+
+    // element variables
+    EditText name, servingSize, fats, carbohydrates, sugar, fibre;
+    Button done;
+    // Firebase
+    DatabaseReference databaseFoods;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic__input);
+
+        // add all the edit text
+        name = (EditText) findViewById(R.id.basicInputName);
+        servingSize = (EditText) findViewById(R.id.basicInputServing);
+        fats = (EditText) findViewById(R.id.basicInputFat);
+        carbohydrates = (EditText) findViewById(R.id.basicInputCarbs);
+        sugar = (EditText) findViewById(R.id.basicInputSugar);
+        fibre = (EditText) findViewById(R.id.basicInputFibre);
+        // submit button
+        done = (Button) findViewById(R.id.basicInputDone);
+        // database reference
+        databaseFoods = FirebaseDatabase.getInstance().getReference().child("Food");
     }
+
 }
---
+++ app/src/main/java/com/example/diabeticmealtracker/Food.java
@@ -0,0 +1,5 @@
+package com.example.diabeticmealtracker;
+
+public class Food {
+    private String name, servingSize, fat, carbohydrates, sugar, fibre;
+}
