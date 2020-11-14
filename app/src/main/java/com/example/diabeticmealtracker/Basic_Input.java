9e3e4df2fc4062f7cfb36f28a2ee602f6b85f40d
FredWei-Hub <fredwei2001@gmail.com>
2020-11-14 07:06:17 -0500

    allowed basic input to add data to firebase

--- app/src/main/java/com/example/diabeticmealtracker/Basic_Input.java
+++ app/src/main/java/com/example/diabeticmealtracker/Basic_Input.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -13,10 +15,11 @@ import com.google.firebase.database.FirebaseDatabase;
 public class Basic_Input extends AppCompatActivity {
 
     // element variables
-    EditText name, servingSize, fats, carbohydrates, sugar, fibre;
+    EditText txtName, txtServingSize, txtFats, txtCarbohydrates, txtSugar, txtFibre;
     Button done;
     // Firebase
     DatabaseReference databaseFoods;
+    Food food;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +27,41 @@ public class Basic_Input extends AppCompatActivity {
         setContentView(R.layout.activity_basic__input);
 
         // add all the edit text
-        name = (EditText) findViewById(R.id.basicInputName);
-        servingSize = (EditText) findViewById(R.id.basicInputServing);
-        fats = (EditText) findViewById(R.id.basicInputFat);
-        carbohydrates = (EditText) findViewById(R.id.basicInputCarbs);
-        sugar = (EditText) findViewById(R.id.basicInputSugar);
-        fibre = (EditText) findViewById(R.id.basicInputFibre);
+        txtName = (EditText) findViewById(R.id.basicInputName);
+        txtServingSize = (EditText) findViewById(R.id.basicInputServing);
+        txtFats = (EditText) findViewById(R.id.basicInputFat);
+        txtCarbohydrates = (EditText) findViewById(R.id.basicInputCarbs);
+        txtSugar = (EditText) findViewById(R.id.basicInputSugar);
+        txtFibre = (EditText) findViewById(R.id.basicInputFibre);
         // submit button
         done = (Button) findViewById(R.id.basicInputDone);
         // database reference
+        food = new Food();
         databaseFoods = FirebaseDatabase.getInstance().getReference().child("Food");
+        // done button
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // parsing the input from the input fields
+                String name = (String) txtName.getText().toString().trim();
+                float servingSize = Float.parseFloat(txtServingSize.getText().toString().trim());
+                float fats = Float.parseFloat(txtFats.getText().toString().trim());
+                float carbohydrates = Float.parseFloat(txtCarbohydrates.getText().toString().trim());
+                float sugar = Float.parseFloat(txtSugar.getText().toString().trim());
+                float fibre = Float.parseFloat(txtFibre.getText().toString().trim());
+                // setting the field inputs into the food object
+                food.setName(name);
+                food.setServingSize(servingSize);
+                food.setFat(fats);
+                food.setCarbohydrates(carbohydrates);
+                food.setSugar(sugar);
+                food.setFibre(fibre);
+                // push food object onto firebase
+                databaseFoods.child(name).setValue(food);
+                // notification saying the input has been successfully added to firebase
+                Toast.makeText(Basic_Input.this, "Input Successful", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/diabeticmealtracker/Food.java
+++ app/src/main/java/com/example/diabeticmealtracker/Food.java
@@ -1,5 +1,54 @@
 package com.example.diabeticmealtracker;
 
 public class Food {
-    private String name, servingSize, fat, carbohydrates, sugar, fibre;
+    private String _name;
+    private float _servingSize, _fat, _carbohydrates, _sugar, _fibre;
+
+    // constructor
+    public Food(){
+    }
+
+    // setter and getter
+    // name
+    public String getName (){
+        return _name;
+    }
+    public void setName (String name){
+        _name = name;
+    }
+    // serving size
+    public float getServingSize(){
+        return _servingSize;
+    }
+    public void setServingSize (float servingSize){
+        _servingSize = servingSize;
+    }
+    // fat
+    public float getFat(){
+        return _fat;
+    }
+    public void setFat (float fat){
+        _fat = fat;
+    }
+    // carbs
+    public float getCarbohydrates(){
+        return _carbohydrates;
+    }
+    public void setCarbohydrates (float carbohydrates){
+        _carbohydrates = carbohydrates;
+    }
+    // sugar
+    public float getSugar(){
+        return _sugar;
+    }
+    public void setSugar (float sugar){
+        _sugar = sugar;
+    }
+    // fibre
+    public float getFibre(){
+        return _fibre;
+    }
+    public void setFibre (float fibre){
+        _fibre = fibre;
+    }
 }
