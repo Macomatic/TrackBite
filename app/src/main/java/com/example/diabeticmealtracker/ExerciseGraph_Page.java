9a213cbd737a86c3e0e11eb6f01fb441cdc9a773
Marco Vethanayagam <29697062+Macomatic@users.noreply.github.com>
2020-11-19 17:36:22 -0500

    Updated exercise analysis pages

--- app/src/main/java/com/example/diabeticmealtracker/ExerciseGraph_Page.java
+++ app/src/main/java/com/example/diabeticmealtracker/ExerciseGraph_Page.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ExerciseAnalyse_Page extends AppCompatActivity {
+public class ExerciseGraph_Page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exercise_analyse__page);
+        setContentView(R.layout.activity_exercise_graph__page);
     }
 }
