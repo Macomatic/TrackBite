d668097dc1b1ba3828b045f01db192e7af7a8060
Andy <43015964+andyung17@users.noreply.github.com>
2020-11-19 20:26:17 -0500

    Added pathways and edited the notification pages

--- app/src/main/java/com/example/diabeticmealtracker/Second_Notification_Page.java
+++ app/src/main/java/com/example/diabeticmealtracker/Second_Notification_Page.java
@@ -2,7 +2,9 @@ package com.example.diabeticmealtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Second_Notification_Page extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class Second_Notification_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second__notification__page);
+    }
+
+    public void backButtonClick (View view){
+        Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void mealButtonClick (View view){
+        Intent intent = new Intent (getApplicationContext(), Notifcation_Setup_Meal.class);
+        startActivity(intent);
+    }
+
+    public void exercisesButtonClick (View view){
+        Intent intent = new Intent (getApplicationContext(), Notifcation_setup_exercise.class);
+        startActivity(intent);
     }
 }
