31aeb4cfca5c8ccf832b2052ba7dd14173874e71
MichaelBosciglio <55165965+MichaelBosciglio@users.noreply.github.com>
2020-11-20 18:50:09 -0500

    Successful Exercise Input
    Designed Successful Exercise Input Page

--- app/src/main/java/com/example/diabeticmealtracker/SuccessExerciseInput_Page.java
+++ app/src/main/java/com/example/diabeticmealtracker/SuccessExerciseInput_Page.java
@@ -2,7 +2,9 @@ package com.example.diabeticmealtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SuccessExerciseInput_Page extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SuccessExerciseInput_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_exercise_input__page);
+    }
+    public void finishExerciseEntry (View view){
+        Intent intent = new Intent (getApplicationContext(), Exercise_Page.class);
+        startActivity(intent);
     }
 }
