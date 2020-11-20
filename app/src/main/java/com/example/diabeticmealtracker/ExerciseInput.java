ce27583236d8ed8cf09fd608f22fce437799d9ce
Marco Vethanayagam <29697062+Macomatic@users.noreply.github.com>
2020-11-20 17:47:22 -0500

    Added logic to ExerciseInput

--- app/src/main/java/com/example/diabeticmealtracker/ExerciseInput.java
+++ app/src/main/java/com/example/diabeticmealtracker/ExerciseInput.java
@@ -17,13 +17,29 @@ import java.util.List;
 
 public class ExerciseInput extends AppCompatActivity {
 
+    private double weight; // kg
+    private double height; // cm
+    private int age; // yrs
+    private String sex; // male or female
+    private double duration; // hrs
+    private String currActivity;
+    private String currSpeed;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.weight = 60.0;
+        this.height = 170.0;
+        this.age = 50;
+        this.sex = "Female";
+        this.duration = 3.0;
+        this.currActivity = "";
+        this.currSpeed = "";
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_input);
 
         Spinner actSpinner = (Spinner) findViewById(R.id.activitySpinner);
-        Spinner unitSpinner = (Spinner) findViewById(R.id.walkingDistUnitSpinner);
         Spinner bikeSpinner = (Spinner) findViewById(R.id.bikingSpinner);
 
         // List of Activities
@@ -31,10 +47,6 @@ public class ExerciseInput extends AppCompatActivity {
         activityArray.add("Biking");
         activityArray.add("Walking");
         activityArray.add("Running");
-
-        List<String> unitArray = new ArrayList<>();
-        unitArray.add("KM");
-        unitArray.add("Steps");
 
         List<String> bikingArray = new ArrayList<>();
         bikingArray.add("Slow");
@@ -44,35 +56,23 @@ public class ExerciseInput extends AppCompatActivity {
 
         // Array adapter (context, layout of spinner, and array values in the spinner)
         ArrayAdapter<String> actAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, activityArray);
-        ArrayAdapter<String> unitAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, unitArray);
         ArrayAdapter<String> bikingAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, bikingArray);
 
         // set drop down menu
         actAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         bikingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         actSpinner.setAdapter(actAdapter);
-        unitSpinner.setAdapter(unitAdapter);
         bikeSpinner.setAdapter(bikingAdapter);
 
         // Gets all dynamic elements
-        TextView numOfSteps = (TextView) findViewById(R.id.walkingText);
-        TextView walkingDistText = (TextView) findViewById(R.id.walkingDistText);
+
 
         actSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
-                if (activityArray.get(pos).equals("Biking")) {
-                    numOfSteps.setVisibility(View.GONE);
-                    walkingDistText.setVisibility(View.GONE);
-                    unitSpinner.setVisibility(View.GONE);
-                }
-                else {
-                    numOfSteps.setVisibility(View.VISIBLE);
-                    walkingDistText.setVisibility(View.VISIBLE);
-                    unitSpinner.setVisibility(View.VISIBLE);
-                }
+                currActivity = activityArray.get(pos);
+                currSpeed = bikingArray.get(pos);
             }
 
             public void onNothingSelected(AdapterView<?> adapterView) {
@@ -82,13 +82,62 @@ public class ExerciseInput extends AppCompatActivity {
         });
     }
 
+    private double BMR(){
+        //https://www.omnicalculator.com/health/bmr#:~:text=This%20BMR%20formula%20is%20as,males%20and%20%2D161%20for%20females.
+        double value = 0;
+        value = 10 * this.weight + 6.25 * this.height - 5 * this.age;
 
+        if (this.sex.equals("Male")){
+            value+= 5;
+        }
+        else{
+            value-=161;
+        }
+
+        return value;
+    }
+
+    private double METS (){
+        if (currActivity.equals("Biking")) {
+            if (this.currSpeed.equals("Slow")){
+                return 3.5;
+            }
+            else if (this.currSpeed.equals("Moderate")){
+                return 5.8;
+            }
+            else{
+                return 8;
+            }
+        }
+        else if (currActivity.equals("Walking")) {
+            if (this.currSpeed.equals("Slow")) {
+                return 2;
+            }
+            else if (this.currSpeed.equals("Moderate")) {
+                return 2.9;
+            }
+            else {
+                return 3.5;
+            }
+        }
+        else { // Running
+            if (this.currSpeed.equals("Slow")) {
+                return 11;
+            }
+            else if (this.currSpeed.equals("Moderate")) {
+                return 11.7;
+            }
+            else {
+                return 12.5;
+            }
+        }
+
+    }
 
     public void backExerciseInput (View view){
         Intent intent = new Intent (getApplicationContext(), Exercise_Page.class);
         startActivity(intent);
     }
-
 
     public void doneInput (View view){
         finish();
