9331affd327ff74cb2398e6a45e59f3cb8617977
MichaelBosciglio <55165965+MichaelBosciglio@users.noreply.github.com>
2020-11-26 23:41:20 -0500

    Firebase Profile
    Can now push profile data to firebase

--- app/src/main/java/com/example/diabeticmealtracker/Profile.java
+++ app/src/main/java/com/example/diabeticmealtracker/Profile.java
@@ -4,7 +4,7 @@ public class Profile {
     private String _name;
     private int _age;
     private String _sex;
-    private float _height, _weight, 
+    private float _height, _weight;
 
     // constructor
     public Profile(){
@@ -33,12 +33,8 @@ public class Profile {
     public float getHeight() {
       return _height;
     }
-    public void setHeight(float height) {
-      _height = height
-    }
-    public float getWeight() {
-      return _weight;
-    }
+    public void setHeight(float height) { _height = height; }
+    public float getWeight() { return _weight; }
     public void setWeight(float weight) {
       _weight = weight;
     }
--- app/src/main/java/com/example/diabeticmealtracker/ProfilePage.java
+++ app/src/main/java/com/example/diabeticmealtracker/ProfilePage.java
@@ -1,18 +1,40 @@
 package com.example.diabeticmealtracker;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.view.View;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+
 public class ProfilePage extends AppCompatActivity {
+
+    private double weight; // kg
+    private double height; // cm
+    private int age; // yrs
+    private String sex; // male or female
+
+    Profile profile;
+    DatabaseReference databaseProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+        profile = new Profile();
+        databaseProfile = FirebaseDatabase.getInstance().getReference("Profile");
     }
+
     public void backMyProfileClick (View view){
         Intent intent = new Intent (getApplicationContext(), MainActivity.class);
         startActivity(intent);
