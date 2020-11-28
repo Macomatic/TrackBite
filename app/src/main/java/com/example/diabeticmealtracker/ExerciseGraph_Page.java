28cb36e3472af40b6d9fb927df91fe0bceb10f76
Marco Vethanayagam <29697062+Macomatic@users.noreply.github.com>
2020-11-28 12:47:21 -0500

    Added exercise analysis page functionality

--- app/src/main/java/com/example/diabeticmealtracker/ExerciseGraph_Page.java
+++ app/src/main/java/com/example/diabeticmealtracker/ExerciseGraph_Page.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class ExerciseGraph_Page extends AppCompatActivity {
 
@@ -13,6 +14,19 @@ public class ExerciseGraph_Page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_graph__page);
     }
+
+    protected void onStart() {
+        super.onStart();
+        Bundle extra = getIntent().getExtras();
+        String[] values = extra.getStringArray("values");
+
+        TextView timeRange = (TextView) findViewById(R.id.activityTimeRange);
+        TextView exerciseAnalysis = (TextView) findViewById(R.id.exerciseAnalysis);
+
+        timeRange.setText(values[0]);
+        exerciseAnalysis.setText(values[1]);
+    }
+
     public void backExerciseGraphPage (View view){
         Intent intent = new Intent (getApplicationContext(), ExerciseAnalysis_Page.class);
         startActivity(intent);
