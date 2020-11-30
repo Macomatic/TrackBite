984f2e954fbb8635e1cd4b572fb29e2e115ae333
MichaelBosciglio <55165965+MichaelBosciglio@users.noreply.github.com>
2020-11-30 16:58:27 -0500

    UI
    UI

--- app/src/main/java/com/example/diabeticmealtracker/Data_Analysis_Page.java
+++ app/src/main/java/com/example/diabeticmealtracker/Data_Analysis_Page.java
@@ -8,9 +8,12 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.DatabaseError;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class Data_Analysis_Page extends AppCompatActivity {
 
@@ -18,7 +21,11 @@ public class Data_Analysis_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data__analysis__page);
+//        Switch graphSwitch = (Switch) findViewById(R.id.graphSwitch);
+//        graphSwitch.setTextOn("Pie Graph");
+//        graphSwitch.setTextOff("Line Graph");
     }
+
     public void backDataAnalysis (View view){
         finish();
     }
