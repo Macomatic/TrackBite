2412151cf6c69a38e87b305b03087c9c63302ee4
MichaelBosciglio <55165965+MichaelBosciglio@users.noreply.github.com>
2020-11-30 17:41:38 -0500

    UI
    UI

---
+++ app/src/main/java/com/example/diabeticmealtracker/CanadaFoodGuideReference.java
@@ -0,0 +1,14 @@
+package com.example.diabeticmealtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CanadaFoodGuideReference extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canada_food_guide_reference);
+    }
+}
