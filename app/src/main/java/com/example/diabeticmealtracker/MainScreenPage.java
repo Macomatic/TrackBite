6ac00adf4e30d7d6320070bb33f4011abb31ea07
MichaelBosciglio <55165965+MichaelBosciglio@users.noreply.github.com>
2020-11-30 18:44:45 -0500

    Sign Out Functionality
    Created a Sign Out button and functionality for the button

--- app/src/main/java/com/example/diabeticmealtracker/MainScreenPage.java
+++ app/src/main/java/com/example/diabeticmealtracker/MainScreenPage.java
@@ -1,18 +1,22 @@
 package com.example.diabeticmealtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.rpc.context.AttributeContext;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainScreenPage extends AppCompatActivity {
-
+    private GoogleApiClient mGoogleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen_page);
+
     }
 
     public void inputButtonClick (View view){
@@ -34,5 +38,9 @@ public class MainScreenPage extends AppCompatActivity {
         Intent intent = new Intent (getApplicationContext(), ProfilePage.class);
         startActivity(intent);
     }
+    public void signOut (View view){
+        finish();
+    }
+
 
 }
